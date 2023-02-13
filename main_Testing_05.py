e46404a27a0a056c9f99a61127dff969756a16ec
Kowalk <ul1021@hs-woe.de>
2023-02-13 17:13:50 +0100

    minor changes

--- main_Testing_05.py
+++ main_Testing_05.py
@@ -21,11 +21,13 @@ NUM_SAMPLES = 5
 BATCH_SIZE = 1
 MAX_THETA = 360.0
 NUM_CLASSES = 72
+MAX_CHANNELS = 15
 NUM_WORKERS = 1
 
 LIST_SNR = [10]
 LIST_T60 = [0.50]
 LIST_UNCERTAINTY = [0.00]
+LIST_AUGMENTATION_STYLE = ['repeat_last', 'repeat_all', 'repeat_roll']
 
 BASE_DIR_ML = os.getcwd()
 SPEECH_DIR = "../LibriSpeech/test-clean"
@@ -71,7 +73,8 @@ PARAMETERS = {'base_dir': BASE_DIR_ML,
               'mask_percentile': None,
               'min_sensors': 5,
               'max_sensors': 5,
-              'augmentation_style': 'roll'}
+              'num_channels': MAX_CHANNELS,
+              'augmentation_style': 'None'} # repeat_last, repeat_all, repeat_roll
 
 
 def boolean_string(s):
@@ -101,13 +104,16 @@ if __name__ == '__main__':
 
     for SNR in LIST_SNR:
         for T60 in LIST_T60:
+            for AUGMENTATION_STYLE in LIST_AUGMENTATION_STYLE:
                 for UNCERTAINTY in LIST_UNCERTAINTY:
 
                         PARAMETERS['min_snr'] = SNR
                         PARAMETERS['max_snr'] = SNR
                         PARAMETERS['min_rt_60'] = T60
                         PARAMETERS['max_rt_60'] = T60
+                        PARAMETERS['augmentation_style'] = AUGMENTATION_STYLE
                         PARAMETERS['max_uncertainty'] = UNCERTAINTY
+                        
                         file_name = Evaluation.get_filename(trained_net, SNR, T60, UNCERTAINTY, PARAMETERS)
                         os.system('color')
                         print(colored(f'Testing: {file_name}', 'grey'))
